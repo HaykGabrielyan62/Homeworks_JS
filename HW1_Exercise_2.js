68123c5adb0ac3e0b676b04368ef4c4fabc27599
HaykGabrielyan62 <60357080+HaykGabrielyan62@users.noreply.github.com>
2020-02-04 19:43:07 +0400

    Add files via upload

---
+++ HW1_Exercise_2.js
@@ -0,0 +1,7 @@
+//Exercise 2
+
+var a = 50;
+var b = 90;
+var c = 180 - (a + b);
+
+alert (c)
---
+++ HW1_Exercise_4.js
@@ -0,0 +1,8 @@
+//Exercise 4
+
+var n = 1002;
+var m = n%10;
+if (m != 0) {
+alert (m + '100')
+}
+else {alert (n)}
