f8c74d69c2b8745e96291f2b364ebe324794f72c
HaykGabrielyan62 <60357080+HaykGabrielyan62@users.noreply.github.com>
2020-03-01 16:06:23 +0400

    Update HW4_Exercise_1.js

--- Homework_4/HW4_Exercise_1.js
+++ Homework_4/HW4_Exercise_1.js
@@ -1,11 +1,38 @@
+//1
+
+function fibonaci (n) {
 let fibArray = [0,1];
-let n = 7;
-for(i = 2; i <=n+1; i++)
-{
-  if (fibArray[i-1]<=n) {
-fibArray[i]= fibArray[i-1] + fibArray[i-2];
- }
+for(i=2; i<=n+2; i++) {
+  if ((fibArray[i-1] + fibArray[i-2]) < n) {
+fibArray[i] = fibArray[i-1] + fibArray[i-2];  }
 }
-fibArray.pop();
-console.log(fibArray);
+  return fibArray;
+}
+console.log(fibonaci(12)); 
+
+
+
+
+//2
+function fibonaci (n) {
+let fibArray = [0,1];
+for(i=2; true; i++){
+let nextToPush = fibArray[i-1] + fibArray[i-2];
+if (n<=nextToPush) {
+return fibArray;
+    } else {
+fibArray.push(nextToPush);}
+  }
+}
+console.log(fibonaci(12)); 
+
+
+
+
+
+
+
+
+
+
 
